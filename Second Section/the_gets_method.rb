38984639186be4b7b3589d1fff259c9cf87e4752
Omar Chenge <carlos.chenge@kueski.com>
2022-02-09 11:56:02 -0600

    Update the_gets_method.rb
    fixing string error

--- Second Section/the_gets_method.rb
+++ Second Section/the_gets_method.rb
@@ -4,4 +4,4 @@ name = gets.chomp
 puts "Great! What's your age?"
 age = gets.chomp.to_i
 
-puts "Cool, so your name is ${name} and you are #{age} years old"
+puts "Cool, so your name is #{name} and you are #{age} years old"
